092cf7c896f6f7312d6c3b5b8c8d5846df871ef2
Kunal sharan <codedragonks07@gmail.com>
2024-04-24 04:55:14 +0530

    c

--- app/ragebooking/page.jsx
+++ app/ragebooking/page.jsx
@@ -21,7 +21,7 @@ export default function AppointmentGrid() {
     <div className="relative w-screen h-screen">
       <div className="flex flex-row w-full py-8 px-4 gap-12 bg-white-green">
         <div className="w-full">
-          <strong className="text-xl ">
+          <strong className="text-xl">
             Instructions for a Smash Room Experience:
           </strong>
           <div className="w-full break-words text-black text-sm overflow-y-auto max-h-screen py-4">
@@ -32,50 +32,50 @@ export default function AppointmentGrid() {
                 shoe cover, and protective suit if available, to protect
                 yourself from flying debris.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Review Rules:</strong> Listen carefully to the rules and
                 guidelines provided by us. This typically includes instructions
                 on where and how to smash items safely.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Enter the Room:</strong> Step into the smash room and
                 familiarize yourself with the space. Take a moment to adjust to
                 the environment and mentally prepare for your session.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Release Your Stress:</strong> Once ready, start
                 smashing! Focus on releasing your stress and pent-up emotions by
                 hitting the items with controlled force. Be mindful of your
                 surroundings and avoid reckless behavior.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Take Breaks:</strong> If you feel overwhelmed or tired,
                 take a break. Step away from the smashing area, breathe deeply,
                 and relax before resuming your session.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Finish Your Session:</strong> When your session is over
-                or you've smashed all the items, exit the room calmly. Remove
-                your safety gear and return it to the designated area.
+                or you&apos;ve smashed all the items, exit the room calmly.
+                Remove your safety gear and return it to the designated area.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Reflect:</strong> Reflect on your experience and how you
                 feel after releasing your stress in the smash room. Take note of
                 any changes in your mood or mindset.
               </li>
-              <br></br>
+              <br />
               <li>
                 <strong>Exit:</strong> Leave the smash room area and return any
                 equipment or gear to us. Share your feedback to help improve the
                 experience for future visitors.
               </li>
-              <br></br>
+              <br />
             </ul>
           </div>
         </div>
@@ -88,12 +88,12 @@ export default function AppointmentGrid() {
               <strong>Are you tired of bottling up your stress?</strong> Do you
               need a release from the pressures of everyday life? Welcome to our
               Smash Room, where you can unleash your inner beast and break,
-              smash, and destroy to your heart's content! Our Smash Room offers
-              a safe and controlled environment where you can let loose and
-              destroy items without fear of consequences. Whether you're feeling
-              overwhelmed at work, dealing with relationship issues, or just
-              need a break from the mundane, our Smash Room is the perfect place
-              to blow off steam.
+              smash, and destroy to your heart&apos;s content! Our Smash Room
+              offers a safe and controlled environment where you can let loose
+              and destroy items without fear of consequences. Whether
+              you&apos;re feeling overwhelmed at work, dealing with relationship
+              issues, or just need a break from the mundane, our Smash Room is
+              the perfect place to blow off steam.
             </div>
             <div className="text-black text-3xl font-bold py-4">
               Available Timings
--- app/termsAndConditions/page.js
+++ app/termsAndConditions/page.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const TermsAndConditions = () => {
   return (
     <div className="bg-white-green">
@@ -20,51 +22,54 @@ const TermsAndConditions = () => {
         <br /> <br />
         <strong className="text-black">2. Product Information</strong>
         <br />
-        <strong>2.1 Accuracy: </strong> We strive to provide accurate and up-to-date information
-        about our products, including descriptions, images, pricing, and
-        availability. However, we do not warrant that the information is
-        error-free or complete, and we reserve the right to correct any errors,
-        inaccuracies, or omissions without prior notice.
+        <strong>2.1 Accuracy: </strong> We strive to provide accurate and
+        up-to-date information about our products, including descriptions,
+        images, pricing, and availability. However, we do not warrant that the
+        information is error-free or complete, and we reserve the right to
+        correct any errors, inaccuracies, or omissions without prior notice.
         <br />
-        <strong>2.2 Product Representation:</strong> The images of our products on the Website
-        are for illustrative purposes only and may differ slightly from the
-        actual products due to variations in color, size, or other attributes.
-        We make every effort to accurately represent our products, but we cannot
-        guarantee that your device's display will accurately reflect the actual
-        product's appearance.
+        <strong>2.2 Product Representation:</strong> The images of our products
+        on the Website are for illustrative purposes only and may differ
+        slightly from the actual products due to variations in color, size, or
+        other attributes. We make every effort to accurately represent our
+        products, but we cannot guarantee that your device&apos;s display will
+        accurately reflect the actual product&apos;s appearance.
         <br />
         <br />
         <strong className="text-black">3. Ordering and Payment</strong>
         <br />
-        <strong>3.1 Order Placement:</strong> When you place an order on our Website, it
-        constitutes an offer to purchase the selected product(s) in accordance
-        with these Terms. We reserve the right to accept or reject your order
-        for any reason, including but not limited to product availability,
-        errors in pricing or product information, or fraudulent activity.
+        <strong>3.1 Order Placement:</strong> When you place an order on our
+        Website, it constitutes an offer to purchase the selected product(s) in
+        accordance with these Terms. We reserve the right to accept or reject
+        your order for any reason, including but not limited to product
+        availability, errors in pricing or product information, or fraudulent
+        activity.
         <br />
-        <strong>3.2 Pricing:</strong> The prices displayed on the Website are in Indian Rupees
-        (INR) and are subject to applicable taxes, unless otherwise stated. We
-        reserve the right to change the prices at any time without prior notice,
-        but such changes will not affect orders that have already been accepted.
+        <strong>3.2 Pricing:</strong> The prices displayed on the Website are in
+        Indian Rupees (INR) and are subject to applicable taxes, unless
+        otherwise stated. We reserve the right to change the prices at any time
+        without prior notice, but such changes will not affect orders that have
+        already been accepted.
         <br />
-        <strong>3.3 Payment:</strong> We accept various payment methods as indicated on the
-        Website. You agree to provide accurate and complete payment information
-        and authorize us to charge the total amount of your order, including
-        taxes and shipping fees, to the provided payment method.
+        <strong>3.3 Payment:</strong> We accept various payment methods as
+        indicated on the Website. You agree to provide accurate and complete
+        payment information and authorize us to charge the total amount of your
+        order, including taxes and shipping fees, to the provided payment
+        method.
         <br />
         <br />
         <strong className="text-black">4. Shipping and Delivery</strong>
         <br />
-        <strong>4.1 Shipping Policy:</strong> We will make reasonable efforts to process and ship
-        your order within the estimated timeframe specified on the Website.
-        However, we do not guarantee delivery dates and are not responsible for
-        any delays or damages caused by the shipping carrier or other
-        circumstances beyond our control.
+        <strong>4.1 Shipping Policy:</strong> We will make reasonable efforts to
+        process and ship your order within the estimated timeframe specified on
+        the Website. However, we do not guarantee delivery dates and are not
+        responsible for any delays or damages caused by the shipping carrier or
+        other circumstances beyond our control.
         <br />
-        <strong>4.2 Delivery Address:</strong> It is your responsibility to provide an accurate
-        and complete delivery address. We will not be liable for any losses or
-        delays resulting from incorrect or incomplete address information
-        provided by you.
+        <strong>4.2 Delivery Address:</strong> It is your responsibility to
+        provide an accurate and complete delivery address. We will not be liable
+        for any losses or delays resulting from incorrect or incomplete address
+        information provided by you.
         <br />
         <br />
         <strong className="text-black">5. Returns and Refunds</strong>
@@ -76,31 +81,33 @@ const TermsAndConditions = () => {
         <br />
         <strong className="text-black">6. Intellectual Property</strong>
         <br />
-        <strong>6.1 Ownership:</strong> All content on the Website, including text, graphics,
-        logos, images, videos, and software, is the property of Frustration
-        Frenzy™ or its licensors and is protected by Indian and international
-        copyright, trademark, and other intellectual property laws.
+        <strong>6.1 Ownership:</strong> All content on the Website, including
+        text, graphics, logos, images, videos, and software, is the property of
+        Frustration Frenzy™ or its licensors and is protected by Indian and
+        international copyright, trademark, and other intellectual property
+        laws.
         <br />
-        <strong>6.2 Limited License:</strong> We grant you a limited, non-exclusive,
-        non-transferable, and revocable license to access and use the Website
-        for personal, non-commercial purposes, subject to these Terms. You may
-        not reproduce, distribute, modify, or create derivative works based on
-        the Website or its content without our prior written consent.
+        <strong>6.2 Limited License:</strong> We grant you a limited,
+        non-exclusive, non-transferable, and revocable license to access and use
+        the Website for personal, non-commercial purposes, subject to these
+        Terms. You may not reproduce, distribute, modify, or create derivative
+        works based on the Website or its content without our prior written
+        consent.
         <br />
         <br />
         <strong className="text-black">7. Disclaimer of Warranties</strong>
         <br />
-        <strong>7.1 Website "As Is":</strong> The Website and its content are provided on an "as
-        is" and "as available" basis, without any warranties of any kind,
-        whether express or implied. We disclaim all warranties, including but
-        not limited to implied warranties of merchantability, fitness for a
-        particular purpose, and non-infringement.
+        <strong>7.1 Website "As Is":</strong> The Website and its content are
+        provided on an "as is" and "as available" basis, without any warranties
+        of any kind, whether express or implied. We disclaim all warranties,
+        including but not limited to implied warranties of merchantability,
+        fitness for a particular purpose, and non-infringement.
         <br />
-        <strong>7.2 Product Disclaimer:</strong> Frustration Frenzy™ is not responsible for any
-        adverse effects or allergic reactions that may result from the use of
-        our products. It is your responsibility to review the product
-        ingredients and consult with a healthcare professional if you have any
-        concerns or medical conditions.
+        <strong>7.2 Product Disclaimer:</strong> Frustration Frenzy™ is not
+        responsible for any adverse effects or allergic reactions that may
+        result from the use of our products. It is your responsibility to review
+        the product ingredients and consult with a healthcare professional if
+        you have any concerns or medical conditions.
         <br />
         <br />
         <strong className="text-black">8. Limitation of Liability</strong>
@@ -129,7 +136,9 @@ const TermsAndConditions = () => {
         any modifications, you accept and agree to the revised Terms.
         <br />
         <br />
-        <strong className="text-black">11. Governing Law and Jurisdiction</strong>
+        <strong className="text-black">
+          11. Governing Law and Jurisdiction
+        </strong>
         <br />
         These Terms shall be governed by and construed in accordance with the
         laws of India. Any disputes arising out of or in connection with these
