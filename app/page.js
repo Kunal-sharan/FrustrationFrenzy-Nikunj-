a384bb549c486a81d2855cbab084fc4c3b8a04e6
Kunal sharan <codedragonks07@gmail.com>
2024-04-24 05:09:37 +0530

    u

--- app/page.js
+++ app/page.js
@@ -1,7 +1,15 @@
+"use client"
+import { useEffect } from "react";
 import MainPage from "./mainpage/page";
 import Navbar from "./navbar/Navbar";
 
 export default function Home() {
+  useEffect(() => {
+    if (!window.location.hash) {
+      window.location.hash = "loaded";
+      window.location.reload();
+    }
+  }, []);
   return (
     <main>
       {/* <Form/> */}
